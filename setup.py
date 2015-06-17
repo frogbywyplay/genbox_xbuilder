717ad5bc8837f4cccd4b12a6d4fd1c7ff3ce6240
bpereira <bpereira@wyplay.com>
2015-06-17 08:35:49 +0000

    fix version

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ def find_packages(dir):
 
 setup(
     name = "xbuilder",
-    version = "1.2.5",
+    version = "1.2.6",
     description = "Xbuilder tool for genbox",
     author = "Wyplay",
     author_email = "noreply@wyplay.com",

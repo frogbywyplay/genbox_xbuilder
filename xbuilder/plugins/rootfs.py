0060de27371a3197cc1b3d7a0d97c8b08b38a3fa
Olivier DOLE <odole@wyplay.com>
2016-04-21 12:19:48 +0000

    Add "tar_extra_opts" option for prebuilt archive.
    * Due to some security requirements, we may want to add some extra
     options to tar command when creating the prebuilt tarball. It is the
     reason why "tar_extra_opts" option is introduced.

--- xbuilder/plugins/rootfs.py
+++ xbuilder/plugins/rootfs.py
@@ -85,7 +85,10 @@ class XBuilderRootfsPlugin(XBuilderPlugin):
                     self.info('Creating rootfs archive')
                     rootfs_file = '%s-%s_root.tar.%s' % (build_info['pkg_name'], build_info['version'], compression)
                     self.log_fd.flush()
-                    ret = Popen(['tar', 'cfzp', workdir + '/' + rootfs_file, '-C', workdir, 'root'],
+		    tar_cmd = ['tar', 'cfzp', workdir + '/' + rootfs_file, '-C', workdir, 'root']
+		    tar_extra_opts = self.cfg['release']['tar_extra_opts']
+		    if tar_extra_opts: tar_cmd.append(tar_extra_opts)
+		    ret = Popen(tar_cmd,
                         bufsize=-1, stdout=self.log_fd, stderr=self.log_fd, shell=False, cwd=None).wait()
                     if ret != 0:
                         raise XUtilsError("Something went wrong while creating the rootfs archive")

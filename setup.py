2e104ba9f5134905b5a5d40135389dc6cbabeaa6
Olivier DOLE <odole@wyplay.com>
2016-08-09 13:55:44 +0000

    Update version in setup.py.

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ def find_packages(dir):
 
 setup(
     name = "xbuilder",
-    version = "1.2.12",
+    version = "1.2.13",
     description = "Xbuilder tool for genbox",
     author = "Wyplay",
     author_email = "noreply@wyplay.com",

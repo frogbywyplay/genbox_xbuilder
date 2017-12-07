7d1111bfbe5d0c218546ff529c089dd276993c54
gstephan <gstephan@wyplay.com>
2017-12-07 17:56:40 +0100

    Enable parallel xz compression when using xz method

--- xbuilder/plugins/rootfs.py
+++ xbuilder/plugins/rootfs.py
@@ -71,9 +71,11 @@ class XBuilderRootfsPlugin(XBuilderPlugin):
 		        self.info('Creating debuginfo archive')
                         debugfs_file = '%s-%s_debuginfo.tar.%s' % (build_info['pkg_name'], build_info['version'], compression)
     	                self.log_fd.flush()
-                        ret = Popen(['tar', 'cfap',
-                            workdir + '/' + debugfs_file,
-                            '-C', workdir, 'root/usr/lib/debug'],
+			tar_cmd = ['tar', 'cfp', workdir + '/' + debugfs_file, '-C', workdir, 'root/usr/lib/debug']
+		        tar_extra_opts = self.cfg['release']['tar_extra_opts']
+		        if tar_comp_opts: tar_cmd.append(tar_comp_opts)
+		        if tar_extra_opts: tar_cmd.append(tar_extra_opts)
+		    	ret = Popen(tar_cmd,
                             bufsize=-1, stdout=self.log_fd, stderr=self.log_fd, shell=False, cwd=None
                             ).wait()
 			shutil.rmtree(workdir + '/root/usr/lib/debug')
@@ -83,8 +85,9 @@ class XBuilderRootfsPlugin(XBuilderPlugin):
                     self.info('Creating rootfs archive')
                     rootfs_file = '%s-%s_root.tar.%s' % (build_info['pkg_name'], build_info['version'], compression)
                     self.log_fd.flush()
-		    tar_cmd = ['tar', 'cfap', workdir + '/' + rootfs_file, '-C', workdir, 'root']
+		    tar_cmd = ['tar', 'cfp', workdir + '/' + rootfs_file, '-C', workdir, 'root']
 		    tar_extra_opts = self.cfg['release']['tar_extra_opts']
+		    if tar_comp_opts: tar_cmd.append(tar_comp_opts)
 		    if tar_extra_opts: tar_cmd.append(tar_extra_opts)
 		    ret = Popen(tar_cmd,
                         bufsize=-1, stdout=self.log_fd, stderr=self.log_fd, shell=False, cwd=None).wait()

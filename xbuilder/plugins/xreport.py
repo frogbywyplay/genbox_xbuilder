b6d801e57609b8790a892758065f2f183c960cdb
Cyril Jouve <cjouve@wyplay.com>
2018-04-06 16:31:09 +0200

    add depgraph

--- xbuilder/plugins/xreport.py
+++ xbuilder/plugins/xreport.py
@@ -2,25 +2,27 @@
 #
 # Copyright (C) 2006-2014 Wyplay, All Rights Reserved.
 # This file is part of xbuilder.
-# 
+#
 # xbuilder is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # xbuilder is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; see file COPYING.
 # If not, see <http://www.gnu.org/licenses/>.
 #
 #
+from __future__ import absolute_import, with_statement
 
+import bz2
+import contextlib
 import os
-from os.path import exists
 
 from subprocess import Popen
 
@@ -29,50 +31,91 @@ from xbuilder.consts import XBUILDER_REPORT_FILE
 
 from xintegtools.xreport import XReport, XReportXMLOutput
 
-import bz2
+from xutils.xerror import XUtilsError
+
+
+@contextlib.contextmanager
+def env_override(**kwargs):
+    def saveenv():
+        for k in kwargs:
+            try:
+                yield k, os.environ[k]
+            except KeyError:
+                pass
+
+    savedenv = dict(saveenv())
+    os.environ.update(kwargs)
+    yield
+    os.environ.update(savedenv)
+
+
+def reload_portage_and_gentoolkit():
+    import portage
+    reload(portage)
+    import gentoolkit
+    reload(gentoolkit)
+    from threading import Lock
+    settingslock = Lock()  # pylint: disable=unused-variable
+    gentoolkit.settings = portage.config(clone=portage.settings)
+    gentoolkit.porttree = portage.db[portage.root]['porttree']
+    gentoolkit.vartree = portage.db[portage.root]['vartree']
+    gentoolkit.virtuals = portage.db[portage.root]['virtuals']
+    import gentoolkit.package
+    reload(gentoolkit.package)
+
 
 class XBuilderXreportPlugin(XBuilderPlugin):
+    def postbuild(self, build_info):
+        self.info('Generating report with xreport')
+        workdir = self.cfg['build']['workdir']
+        rootdir = os.path.join(workdir, 'root/')
+        xr = XReport(root=rootdir, portage_configroot=rootdir)
+        xr.vdb_create()
+        self.report_file = os.path.join(workdir, XBUILDER_REPORT_FILE)
+        with contextlib.closing(bz2.BZ2File(self.report_file, 'w')) as fd:
+            with env_override(ROOT=rootdir, PORTAGE_CONFIGROOT=rootdir):
+                reload_portage_and_gentoolkit()
+                XReportXMLOutput(errors_only=False).process(xr, fd, True)
 
-        def postbuild(self, build_info):
-                self.info('Generating report with xreport')
-                workdir = self.cfg['build']['workdir']
-                xr = XReport(root = workdir + '/root',
-                             portage_configroot = workdir + '/root')
-                xr.vdb_create()
-                self.report_file = '/'.join([workdir, XBUILDER_REPORT_FILE])
-                fd = bz2.BZ2File(self.report_file, 'w')
-                out = XReportXMLOutput(errors_only=False).process(xr, fd)
-                fd.close()
+        reload_portage_and_gentoolkit()
+        self.info('Generating host-report with xreport')
+        hostxr = XReport()
+        hostxr.vdb_create()
+        self.report_host_file = os.path.join(workdir, 'host-' + XBUILDER_REPORT_FILE)
+        with contextlib.closing(bz2.BZ2File(self.report_host_file, 'w')) as hostfd:
+            XReportXMLOutput(errors_only=False).process(hostxr, hostfd)
+        return self.report_file, self.report_host_file
 
-		hostxr = XReport()
-		hostxr.vdb_create()
-		self.report_host_file = '/'.join([workdir, 'host-' + XBUILDER_REPORT_FILE])
-		hostfd = bz2.BZ2File(self.report_host_file, 'w')
-		out = XReportXMLOutput(errors_only=False).process(hostxr, hostfd)
-		hostfd.close()
-		return self.report_file,self.report_host_file
+    def release(self, build_info):
+        archive = self.cfg['release']['archive_dir']
 
-        def release(self, build_info):
-                archive = self.cfg['release']['archive_dir']
+        dest_dir = '/'.join([
+            archive, build_info['category'], build_info['pkg_name'], build_info['version'], build_info['profile']
+        ])
 
-                dest_dir = "/".join([archive, build_info['category'],
-                        build_info['pkg_name'], build_info['version'],
-                        build_info['profile']])
+        try:
+            os.makedirs(dest_dir)
+        except OSError:
+            if not os.path.exists(dest_dir):
+                raise
 
-                if not exists(dest_dir):
-                        os.makedirs(dest_dir)
+        self.info('Releasing report file')
+        self.log_fd.flush()
+        ret = Popen(['cp', self.report_file, '/'.join([dest_dir, XBUILDER_REPORT_FILE])],
+                    bufsize=-1,
+                    stdout=self.log_fd,
+                    stderr=self.log_fd,
+                    shell=False,
+                    cwd=None).wait()
+        Popen(['cp', self.report_host_file, '/'.join([dest_dir, 'host-' + XBUILDER_REPORT_FILE])],
+              bufsize=-1,
+              stdout=self.log_fd,
+              stderr=self.log_fd,
+              shell=False,
+              cwd=None).wait()
+        if ret != 0:
+            raise XUtilsError('Failed to release the report file')
 
-                self.info('Releasing report file')
-                self.log_fd.flush()
-                ret = Popen(['cp', self.report_file, '/'.join([dest_dir, XBUILDER_REPORT_FILE])],
-                            bufsize=-1, stdout=self.log_fd, stderr=self.log_fd, shell=False, cwd=None
-                           ).wait()
-                ret2 = Popen(['cp', self.report_host_file, '/'.join([dest_dir, 'host-' + XBUILDER_REPORT_FILE])],
-                            bufsize=-1, stdout=self.log_fd, stderr=self.log_fd, shell=False, cwd=None
-                           ).wait()
-                if ret != 0:
-                        raise XUtilsError("Failed to release the report file")
 
 def register(builder):
-        builder.add_plugin(XBuilderXreportPlugin)
-
+    builder.add_plugin(XBuilderXreportPlugin)
